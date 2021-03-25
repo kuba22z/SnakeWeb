f74725925c4a3d00dfa1816e23252341a452c031
kuba22z <your@email.com>
2021-03-25 13:49:32 +0100

    initial commit

---
+++ src/main/java/com/example/snakeweb/SnakewebApplication.java
@@ -0,0 +1,13 @@
+package com.example.snakeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakewebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnakewebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/snakeweb/SnakewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snakeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakewebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
